6f87f5b5122c980f745d9a57df4ace8042aa2ec6
Vesko Vasilev <payvesipal@gmail.com>
2024-08-27 20:25:56 +0300

    move the serialize decorator in the controller scope

--- src/user/auth.controller.ts
+++ src/user/auth.controller.ts
@@ -5,11 +5,11 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dto/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  @Serialize(UserDto)
   register(@Body() createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
   }
