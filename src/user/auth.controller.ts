de710a2194113dc37f04315852aad308f0b7864c
Vesko Vasilev <payvesipal@gmail.com>
2024-08-27 21:36:13 +0300

    implement login

--- src/user/auth.controller.ts
+++ src/user/auth.controller.ts
@@ -3,14 +3,21 @@ import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dto/user.dto';
+import { LoginDto } from './dto/login.dto';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
-@Serialize(UserDto)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  register(@Body() createUserDto: CreateUserDto) {
+  @Serialize(UserDto)
+  register(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.authService.register(createUserDto);
+  }
+
+  @Post('login')
+  login(@Body() loginCredentials: LoginDto): Promise<{ accessToken: string }> {
+    return this.authService.login(loginCredentials);
   }
 }
--- src/user/auth.service.ts
+++ src/user/auth.service.ts
@@ -1,15 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './jwt-payload.interface';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(createUserDto: CreateUserDto): Promise<User> {
     return await this.userService.create(createUserDto);
   }
 
-  login() {}
+  async login(loginCredentials: LoginDto): Promise<{ accessToken: string }> {
+    const { username, password } = loginCredentials;
+
+    const user = await this.userService.findByUsername(username);
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid username or password!');
+    }
+
+    const payload: JwtPayload = { username };
+
+    const accessToken = await this.jwtService.sign(payload);
+    return { accessToken };
+  }
 }
