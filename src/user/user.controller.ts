9c6fd314eebf133a0f2d57da2caab4996e52ad31
Vesko Vasilev <payvesipal@gmail.com>
2024-08-27 08:03:29 +0300

    implement the user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDto } from './dto/user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+@Controller('auth')
+@Serialize(UserDto)
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findUsers(): Promise<User[]> {
+    return this.userService.find();
+  }
+
+  @Get(':id')
+  findUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.create(createUserDto);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.userService.delete(id);
+  }
 }
