788a980b6dd767379a7aee01b74d406c304270e5
Vesko Vasilev <payvesipal@gmail.com>
2024-08-28 22:38:32 +0300

    setup configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,27 @@ import { Module } from '@nestjs/common';
 import { ReportModule } from './report/report.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
   imports: [
-    ReportModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      //Enable auto migrations. Disable this in production
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
     }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          //Enable auto migrations. Disable this in production
+          synchronize: true,
+        };
+      },
+    }),
+    ReportModule,
     UserModule,
   ],
   controllers: [],
